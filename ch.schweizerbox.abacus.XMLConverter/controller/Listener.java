5f1d2ce12a80332780e7528473040152a0cb2e50
Roland <Roland@DESKTOP-OMV0MSV.home>
2016-03-24 07:18:31 +0100

    Projekt FocusListener
    - Meldung beim Speichern ergänzt
    - FocusListener versucht zu implementieren.

---
+++ ch.schweizerbox.abacus.XMLConverter/controller/Listener.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+
+public class Listener {
+
+}
